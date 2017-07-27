aa578ee8d3cb0727c834c9cad7a94de3a979f6a4
Mikael Arguedas <mikael@osrfoundation.org>
2017-07-27 13:12:36 -0400

    Rebase from 'rpm/lunar/class_loader'

--- include/class_loader/class_loader_register_macro.h
+++ include/class_loader/class_loader_register_macro.h
@@ -33,6 +33,8 @@
 #include "class_loader_core.h"
 #include <console_bridge/console.h>
 
+#include "class_loader/console_bridge_compatibility.h"
+
 #define CLASS_LOADER_REGISTER_CLASS_INTERNAL_WITH_MESSAGE(Derived, Base, UniqueID, Message) \
 namespace \
 {\
@@ -43,12 +45,12 @@ namespace \
     ProxyExec##UniqueID() \
     { \
       if(std::string(Message)!="")\
-        logInform("%s", Message);\
+        CONSOLE_BRIDGE_logInform("%s", Message);\
       class_loader::class_loader_private::registerPlugin<_derived, _base>(#Derived, #Base); \
     }\
   };\
   static ProxyExec##UniqueID g_register_plugin_##UniqueID;\
-} 
+}
 
 #define CLASS_LOADER_REGISTER_CLASS_INTERNAL_HOP1_WITH_MESSAGE(Derived, Base, UniqueID, Message) CLASS_LOADER_REGISTER_CLASS_INTERNAL_WITH_MESSAGE(Derived, Base, UniqueID, Message)
 
